185b077eac9901e5c334541ca47b4b93d2ff1adb
agei5poli5 <willburghard@gmail.com>
2020-03-13 23:54:53 -0400

    Added map, set, and check if selected character in first string has occurred

--- main.js
+++ main.js
@@ -2,7 +2,23 @@ const onetoone = (first, second) => {
 	if (first.length !== second.length){
 		return false;
 	}
+    let size = first.length; 
 
+/* map will be the mapping of characters in (key, value) form. set represents the set of characters from the second string that have already occurred.*/
+    let map = new Map();
+    let set = new Array(size);
+    
+    for(let i = 0; i < size; i++){
+
+        let x = first.charAt(i);
+        let y = second.charAt(i);
+      //Check if character from first string has already occurred and if its value in map is equivalent to character from second string
+        if(map.has(x)){
+            if (map.get(x) !== y){
+                return false;
+            }
+        }
+    }
     return true;
 }
 
